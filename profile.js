ed0d9e535bc8c8e5f61206786f23cfd51a240b72
Benedict Lawrence <BTLawrence44@gmail.com>
2021-06-15 16:35:20 +0100

    scrolling has been added

---
+++ profile.js
@@ -0,0 +1,31 @@
+
+document.getElementById('nav_profile').addEventListener('click', function(){
+    document.querySelector('.Profile_header').scrollIntoView({
+        behavior: 'smooth'
+    })
+}) 
+
+document.getElementById('nav_work').addEventListener('click', function(){
+    document.querySelector('.work_header').scrollIntoView({
+        behavior: 'smooth'
+    })
+}) 
+
+document.getElementById('nav_education').addEventListener('click', function(){
+    document.querySelector('.Education_header').scrollIntoView({
+        behavior: 'smooth'
+    })
+})  
+
+document.getElementById('nav_projects').addEventListener('click', function(){
+    document.querySelector('.Projects').scrollIntoView({
+        behavior: 'smooth'
+    })
+})  
+
+document.getElementById('nav_hobbies').addEventListener('click', function(){
+    document.querySelector('.hobbies').scrollIntoView({
+        behavior: 'smooth'
+    })
+})  
+

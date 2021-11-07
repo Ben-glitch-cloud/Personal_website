f9ec0edbfd5682fedf6612c6b26a8aebf5d179a5
Benedict Lawrence <BTLawrence44@gmail.com>
2021-11-07 17:27:42 +0000

    updating nav bar

--- profile.js
+++ profile.js
@@ -1,5 +1,15 @@
 "use strict"; 
 
+document.addEventListener('scroll', function(){
+    if(scrollY >= 377)
+    {
+        document.getElementById('second_nav').style.display = 'flex'
+    } else if(scrollY < 377)
+    {
+        document.getElementById('second_nav').style.display = 'none'
+    }
+})
+
 
 document.getElementById('nav_profile').addEventListener('click', function(){
     document.querySelector('.Profile_header').scrollIntoView({
@@ -31,3 +41,34 @@ document.getElementById('nav_hobbies').addEventListener('click', function(){
     })
 })  
 
+// second navication
+
+document.getElementById('nav_profile_2').addEventListener('click', function(){
+    document.querySelector('.Profile_header').scrollIntoView({
+        behavior: 'smooth'
+    })
+}) 
+
+document.getElementById('nav_work_2').addEventListener('click', function(){
+    document.querySelector('.work_header').scrollIntoView({
+        behavior: 'smooth'
+    })
+}) 
+
+document.getElementById('nav_education_2').addEventListener('click', function(){
+    document.querySelector('.Education_header').scrollIntoView({
+        behavior: 'smooth'
+    })
+})  
+
+document.getElementById('nav_projects_2').addEventListener('click', function(){
+    document.querySelector('.Projects').scrollIntoView({
+        behavior: 'smooth'
+    })
+})  
+
+document.getElementById('nav_hobbies_2').addEventListener('click', function(){
+    document.querySelector('.hobbies').scrollIntoView({
+        behavior: 'smooth'
+    })
+})

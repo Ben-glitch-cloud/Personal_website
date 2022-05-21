923757b1e1de487c976ebb56a83ae77b5167e45c
Ben-glitch-cloud <BTLawrence44@gmail.com>
2022-05-21 11:00:22 +0100

    adding styling to elements

--- profile.js
+++ profile.js
@@ -9,47 +9,39 @@ function ChangeOpacity(element, low, high){ scrollPostion > low && scrollPostion
 
 document.addEventListener('scroll', function(){scrollPostion = window.scrollY})
 
-// Profile animation fade in and out // 
-
 let mainProfile = document.getElementById("main_profile") 
 
-document.addEventListener('scroll', function(){ 
-    if(scrollPostion > 590 && scrollPostion < 967){ 
+document.addEventListener('scroll', function(){  
+    if(scrollPostion > mainProfile.offsetTop - 270  && scrollPostion < mainProfile.offsetTop + 210){ 
         mainProfile.style.animationName = 'Profile_fadeIn'
-        setTimeout(ChangeOpacity(mainProfile, 590, 967), 1200) 
+        setTimeout(ChangeOpacity(mainProfile, mainProfile.offsetTop - 270, mainProfile.offsetTop + 210), 1200) 
     } else {   
         mainProfile.style.animationName = 'Profile_fadeOut'
         mainProfile.style.opacity = 0
     } 
 })  
 
-// project 
-
 let project_one = document.getElementById("project_one")
 let projects_two = document.getElementById("projects_two") 
 let projects_three = document.getElementById("projects_three") 
 
 document.addEventListener('scroll', function(){
-    if(scrollPostion > 997 && scrollPostion < 1590){  
+    if(scrollPostion > project_one.offsetTop - 310 && scrollPostion < project_one.offsetTop + 250){  
 
         setTimeout(function(){
             project_one.style.animationName = 'ProjectFadeIn'  
-            setTimeout(ChangeOpacity(project_one, 997, 1590), 1200)  
+            setTimeout(ChangeOpacity(project_one, project_one.offsetTop - 310, project_one.offsetTop + 250), 1200)  
         }, 0)
 
         setTimeout(function(){ 
             projects_two.style.animationName = 'ProjectFadeIn' 
-            setTimeout(ChangeOpacity(projects_two, 997, 1590), 1200) 
+            setTimeout(ChangeOpacity(projects_two, projects_two.offsetTop - 310, projects_two.offsetTop + 250), 1200) 
         }, 1000)
           
         setTimeout(function(){
             projects_three.style.animationName = 'ProjectFadeIn' 
-            setTimeout(ChangeOpacity(projects_three, 997, 1590), 1200)
+            setTimeout(ChangeOpacity(projects_three, projects_three.offsetTop - 310, projects_three.offsetTop + 250), 1200)
         }, 2000)
-         
-
-        
-       
          
     } else {
         project_one.style.animationName = null
@@ -70,14 +62,14 @@ let work_area_two = document.getElementById('work_area_two')
 let work_area_three = document.getElementById('work_area_three') 
 let work_area_four = document.getElementById('work_area_four')
 
+
 document.addEventListener('scroll', function(){
-    console.log(scrollPostion) 
 
-    // one
+    // one 
 
-    if(scrollPostion > 1590 && scrollPostion < 2160){
+    if(scrollPostion > work_area_one.offsetTop - 300 && scrollPostion < work_area_one.offsetTop){
         work_area_one.style.animationName = 'WorkFadeIn' 
-        setTimeout(ChangeOpacity(work_area_one, 1590, 2160), 1200) 
+        setTimeout(ChangeOpacity(work_area_one, work_area_one.offsetTop - 300, work_area_one.offsetTop), 1200) 
     } else { 
         work_area_one.style.animationName = 'WorkFadeOut'
         work_area_one.style.opacity = 0 
@@ -85,9 +77,9 @@ document.addEventListener('scroll', function(){
 
     // two
 
-    if(scrollPostion > 2160 && scrollPostion < 2460){
+    if(scrollPostion > work_area_two.offsetTop - 300 && scrollPostion < work_area_two.offsetTop){
         work_area_two.style.animationName = 'WorkFadeIn' 
-        setTimeout(ChangeOpacity(work_area_two, 2160, 2460), 1200) 
+        setTimeout(ChangeOpacity(work_area_two, work_area_two.offsetTop - 300, work_area_two.offsetTop), 1200) 
     } else { 
         work_area_two.style.animationName = 'WorkFadeOut'
         work_area_two.style.opacity = 0 
@@ -95,9 +87,9 @@ document.addEventListener('scroll', function(){
 
     // three
 
-    if(scrollPostion > 2460 && scrollPostion < 2830){
+    if(scrollPostion > work_area_three.offsetTop - 200 && scrollPostion < work_area_three.offsetTop + 100){
         work_area_three.style.animationName = 'WorkFadeIn' 
-        setTimeout(ChangeOpacity(work_area_three, 2460, 2830), 1200) 
+        setTimeout(ChangeOpacity(work_area_three, work_area_three.offsetTop - 200, work_area_three.offsetTop + 100), 1200) 
     } else { 
         work_area_three.style.animationName = 'WorkFadeOut'
         work_area_three.style.opacity = 0 
@@ -105,9 +97,9 @@ document.addEventListener('scroll', function(){
 
     // four 
 
-    if(scrollPostion > 2830 && scrollPostion < 3060){
+    if(scrollPostion > work_area_four.offsetTop - 260 && scrollPostion < work_area_four.offsetTop){
         work_area_four.style.animationName = 'WorkFadeIn' 
-        setTimeout(ChangeOpacity(work_area_four, 2890, 3060), 1200) 
+        setTimeout(ChangeOpacity(work_area_four, work_area_four.offsetTop - 260, work_area_four.offsetTop), 1200) 
     } else { 
         work_area_four.style.animationName = 'WorkFadeOut'
         work_area_four.style.opacity = 0 
@@ -120,25 +112,26 @@ document.addEventListener('scroll', function(){
 let Education_area_one = document.getElementById('Education_area_one') 
 let Education_area_two = document.getElementById('Education_area_two')
 
+
 document.addEventListener('scroll', function(){ 
 
     // one
 
-    if(scrollPostion > 3160 && scrollPostion < 3370){
+    if(scrollPostion > Education_area_one.offsetTop - 300 && scrollPostion < Education_area_one.offsetTop){
         Education_area_one.style.animationName = 'EducationFadeIn' 
-        setTimeout(ChangeOpacity(Education_area_one, 3160, 3370), 1200) 
+        setTimeout(ChangeOpacity(Education_area_one, Education_area_one.offsetTop - 300, Education_area_one.offsetTop), 1200) 
     } else {
         Education_area_one.style.animationName = 'EducationFadeOut' 
         Education_area_one.style.opacity = 0
     } 
     
-    // two
+    // two work on this 
 
-    if(scrollPostion > 3370 && scrollPostion < 3600){
+    if(scrollPostion > Education_area_two.offsetTop - 250 && scrollPostion < Education_area_two.offsetTop + 250){
         Education_area_two.style.animationName = 'EducationFadeIn' 
-        setTimeout(ChangeOpacity(Education_area_two, 3370, 3600), 1200) 
+        setTimeout(ChangeOpacity(Education_area_two, Education_area_two.offsetTop - 250, Education_area_two.offsetTop + 250), 1200) 
     } else {
-        Education_area_two.style.animationName = 'EducationFadeOut' 
+        Education_area_two.style.animationName = 'EducationFadeOut'  
         Education_area_two.style.opacity = 0
     } 
 })
